1d5c92c0ff6c8efda4e7bb349935abd49ca496ac
SomiZ <116781266+SomiZ@users.noreply.github.com>
2025-03-02 15:01:23 -0500

    Update Problem1.py

--- session_07/Problem1.py
+++ session_07/Problem1.py
@@ -21,6 +21,6 @@ def divide_numbers():
     num1 = input("Enter first number: ")
     num2 = input("Enter second number: ")
     result = int(num1) / int(num2)
-    print(f"Result: {result}")
+    print("Result:", result)
 
 divide_numbers()
